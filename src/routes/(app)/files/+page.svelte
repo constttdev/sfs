45163504425a5e7383efbd761d18e0fc6fbf295f
constt <mattheo.hering@gmail.com>
2025-02-18 17:19:57 +0100

    style: buttons and txt are now justify between

--- src/routes/(app)/files/+page.svelte
+++ src/routes/(app)/files/+page.svelte
@@ -17,19 +17,24 @@
 			>
 				<File />
 			</div>
-			<div class="font-inter p-2 text-[14px]">
-				<p class="font-semibold">{file.fileName}</p>
-				<p>{byteSize(file.fileSize)}</p>
-				<button
-					formmethod="post"
-					formaction="upload?/deleteFile"
-					class="flex h-10 w-20 cursor-pointer items-center gap-3 rounded-lg bg-red-500 p-1"
-					><Delete class="size-7" /> Delete</button
-				>
-				<input type="hidden" value={file.id} id="recordId" name="recordId" />
-				<button class="flex h-10 w-20 cursor-pointer items-center gap-3 rounded-lg bg-blue-500 p-1"
-					><Share_2 class="size-7" /> Share</button
-				>
+			<div class="font-inter flex w-4xl justify-between p-2 text-[14px]">
+				<div>
+					<p class="font-semibold">{file.fileName}</p>
+					<p>{byteSize(file.fileSize)}</p>
+				</div>
+				<div class="flex gap-4">
+					<button
+						formmethod="post"
+						formaction="upload?/deleteFile"
+						class="flex h-10 w-20 cursor-pointer items-center gap-3 rounded-lg bg-red-500 p-1"
+						><Delete class="size-7" /> Delete</button
+					>
+					<input type="hidden" value={file.id} id="recordId" name="recordId" />
+					<button
+						class="flex h-10 w-20 cursor-pointer items-center gap-3 rounded-lg bg-blue-500 p-1"
+						><Share_2 class="size-7" /> Share</button
+					>
+				</div>
 			</div>
 		</div>
 	{/each}
