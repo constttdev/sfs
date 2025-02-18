d8d0c4c49a7e969863d107eabbb0030c2cf97175
constt <mattheo.hering@gmail.com>
2025-02-18 13:35:41 +0100

    feat: usage of byteSize package feat: file name style: added icon and styled text

--- src/routes/(app)/files/+page.server.ts
+++ src/routes/(app)/files/+page.server.ts
@@ -6,10 +6,19 @@ export const load = async ({ locals }) => {
 	for (const file of files) {
 		const url = locals.pb.files.getURL(file, file.file);
 		const request = await fetch(url, { method: 'GET' });
-		const fileSize = request.headers.get('Content-Length');
+		const fileSize: number | undefined = Number(request.headers.get('Content-Length'));
+		const fileName: string | undefined = String(request.headers.get('Content-Disposition'))
+			.split(';')
+			.pop()
+			?.replaceAll('filename=', '')
+			.trimStart();
 		filesR.push({
+			fileName: fileName,
 			fileSize: fileSize
 		});
+		// request.headers.forEach((value, key) => {
+		// 	console.log(`${key}: ${value}`);
+		// });
 	}
 
 	return { files: filesR };
--- src/routes/(app)/files/+page.svelte
+++ src/routes/(app)/files/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+	import byteSize from 'byte-size';
+	import File from 'lucide-svelte/icons/file';
+
 	let { data } = $props();
 </script>
 
-<div class="flex flex-col">
+<div class="mt-20 flex flex-col gap-4">
 	{#each data.files as file}
-		<div class="p-1">
-			<p>{JSON.stringify(file)}</p>
+		<div class="flex gap-4">
+			<div
+				class="font-inter flex w-13 items-center justify-center rounded-md bg-zinc-200 dark:bg-zinc-900"
+			>
+				<File />
+			</div>
+			<div class="font-inter p-2 text-[14px]">
+				<p class="font-semibold">{file.fileName}</p>
+				<p>{byteSize(file.fileSize)}</p>
+			</div>
 		</div>
 	{/each}
 </div>
